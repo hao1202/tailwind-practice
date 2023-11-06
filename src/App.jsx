5d69ab01bcd3ab538d129ea212e78b87c5266b00
hao1202 <tranvanhao2122003@gmail.com>
2023-11-06 23:54:04 +0700

    One user-card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { UserCard } from "./One/UserCard";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-[#E6EFFA]">
+      <UserCard />
+    </div>
+  );
 }
 
 export default App;
